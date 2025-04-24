3a53032fd073ba8c25e04802b9f53f06da29f8be
Nicholas Mountain <nmountain@unc.edu>
2025-04-23 20:13:04 -0400

    usage explanation

--- tools/fix_tag.py
+++ tools/fix_tag.py
@@ -1,5 +1,36 @@
 #!/usr/bin/python3
 
+"""
+This is a command line search and replace script used in this project to
+fix placeholder tags. 
+The script takes in three command line arguments: 
+1. the file to change
+2. the old tag that will be changed
+3. the new tag that will be added
+It will replace every instance of arg 2 with arg 3
+
+Some example uses:
+tools/fix_tag.py edition.xml "<line" "<lb"
+this changes all instances of the "<line" placeholder tag in edition.xml to the
+correct "<lb"
+
+tools/fix_tag.py edition.xml "<tironianet/>" ""
+this removes all instances of the unnecessary "<tironianet/>" tag and replaces them
+with nothing
+
+Important notes about usage
+Since this is a script, it can be run without the python3 command, but you need to first 
+give it permission to execute.
+If you are in the project's root directory, run:
+chmod +x tools/fix_tag.py
+then you can run the script with the command:
+tools/fix_tag.py [name of file to change] [old text] [replacement text]
+
+VERY IMPORTANT: if your arguments include a tag with the '<' or '>' characters, you MUST
+use quotes around the argument, or your terminal will think you are doing i/o redirection,
+and the script will not work.
+"""
+
 import sys
 
 def main():
